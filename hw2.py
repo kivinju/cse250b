a7d4dd3f92b510a1ee1eb1738aedacf7e2459f91
kivinju <kaz040@eng.ucsd.edu>
2016-01-16 20:29:50 -0800

    finish hw2 read work

---
+++ hw2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from collections import defaultdict
+import numpy as np
+
+
+def read_data_file(filename):
+    data_file = open(filename)
+    dict = {}
+    for line in data_file:
+        l = line.split()
+        docIdx = eval(l[0])
+        wordIdx = eval(l[1])
+        count = eval(l[2])
+        if docIdx not in dict:
+            dict[docIdx] = defaultdict(int)
+        dict[docIdx][wordIdx] += count
+    data_file.close()
+    return dict
+
+
+def read_data_label(filename):
+    data_file = open(filename)
+    dict = defaultdict(int)
+    map = defaultdict(int)
+
+    count = 0
+    for line in data_file:
+        groupId = eval(line)
+        dict[groupId] += 1
+        count += 1
+        map[count] = groupId
+    data_file.close()
+    return dict, map
+
+voc_file = open("data/vocabulary.txt")
+voc_dict = defaultdict(int)
+count = 0
+for line in voc_file:
+    count += 1
+    voc_dict[line] = count
+voc_file.close()
+
+train_data = read_data_file("data/train.data")
+train_label, train_map = read_data_label("data/train.label")
+
+group_num = len(train_label)
+doc_num = len(train_map)
+voc_num = len(voc_dict)
+
+pi = [train_label[groupId] * 1.0 / doc_num for groupId in range(1, 21)]
+# smoothing
+p = np.ones((group_num, voc_num))
+
+for docId in train_data:
+    for vId in train_data[docId]:
+        p[train_map[docId] - 1][vId - 1] += train_data[docId][vId]
+
+for groupId in range(len(p)):
+    group_sum = sum(p[groupId])
+    for vId in range(len(p[groupId])):
+        p[groupId][vId] = p[groupId][vId] / group_sum
+
+
+
+
