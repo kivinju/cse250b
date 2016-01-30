9741d3e958363b44b2aa4932d4eecc5b6c124b29
kivinju <kaz040@eng.ucsd.edu>
2016-01-30 10:56:43 -0800

    add pics

--- hw3.py
+++ hw3.py
@@ -1,4 +1,21 @@
 __author__ = 'zhoukai'
 
 
-ß
+import numpy as np
+import matplotlib.pyplot as plt
+
+# mean = [0, 0]
+# cov = [[9, 0], [0, 1]]
+#
+# x, y = np.random.multivariate_normal(mean, cov, 100).T
+# plt.plot(x, y, 'x')
+# plt.axis('equal')
+# plt.savefig('2a.png')
+
+mean = [0, 0]
+cov = [[1, -0.75], [-0.75, 1]]
+
+x, y = np.random.multivariate_normal(mean, cov, 100).T
+plt.plot(x, y, 'x')
+plt.axis('equal')
+plt.savefig('2b.png')
